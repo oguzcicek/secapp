ef8d5e83ce70791338924f613c4c7dab061c30a4
oguzcicek <soaoflisa@hotmail.com>
2020-12-04 20:59:47 +0300

    A

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  #
+
+
+  get '/post-1', to: redirect('https://www.localworkingtimes.com/uk/catalogue/poundstretcher/poundstretcher-offers-01-30-dec-2020/offers', status: 301)
+
 end
